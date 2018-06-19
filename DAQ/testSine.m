a9c7bd1e781a5156f632c1c14db67d1c7e501f37
Mayank Vanjani <MayankVanjani@172-16-244-211.dynapool.nyu.edu>
2018-06-19 12:09:07 -0400

    Modified_Sine

--- DAQ/testSine.m
+++ DAQ/testSine.m
@@ -15,7 +15,7 @@ addAnalogOutputChannel(daqSession,'Dev1',0,'Voltage');
 
 %% Voltage waveform
 N = Sampling_rate/frequency;
-VDAC = (Vpp/2)*sin(((2*pi/N)*(1:(Sampling_rate/0.01))-(2*pi/N)))'+Voff;
+VDAC = (Vpp/2) * sin(((2*pi/N)*(1:(Sampling_rate/0.01)) - (2*pi/N)))' + Voff;
 
 queueOutputData(daqSession,VDAC);
 startBackground(daqSession);
